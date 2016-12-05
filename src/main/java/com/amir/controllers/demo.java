d5f393d3cdc6c1e7deb7fb5c6cc2af252ddf2f4d
Amir Saadi <amir.hooshang.saadi@gmail.com>
2016-12-05 18:34:44 +0100

    test

--- src/main/java/com/amir/controllers/demo.java
+++ src/main/java/com/amir/controllers/demo.java
@@ -9,31 +9,7 @@ import java.util.Scanner;
  * spring-weblog
  */
 public class demo {
-    public static void main(String[] args) {
-        int amountOfTeams;
-        System.out.println("Enter the number of teams: ");
-        Scanner keyboard =new Scanner(System.in);
-        amountOfTeams=keyboard.nextInt();
-        keyboard.nextLine(); // Consume newline left-over
-        String teamName = "";
-        int x = 1;
-        int c = 2;
-        while(x<=amountOfTeams)
-
-        {
-            System.out.println("What is the name of team " + (x++) + ": ");
-            teamName = keyboard.nextLine();
-            int b = 1;
-
-            for (int a = 1; a < amountOfTeams; ++a) {
-
-                System.out.println("Match " + a++ + ": (" + b++ + " of " + (amountOfTeams - 1) + ") for " + teamName
-                        + " vs. team " + c);
-                c++;
-            }
-        }
-    }
 
 
-
+  
 }
